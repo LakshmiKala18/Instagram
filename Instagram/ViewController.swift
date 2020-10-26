7d6b5c2b820db7c7a2dbaed19a1204d4ef673acf
Lakshmi Kala Pedarla <kala.pedarla18@gmail.com>
2020-10-26 10:13:52 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Lakshmi Kala on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
